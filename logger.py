f603a92c61257de68f0824361e04833fadebf451
zhongqin0820 <zhongqin0820@163.com>
2019-08-31 21:33:20 +0800

    changed logger

--- logger.py
+++ logger.py
@@ -1,17 +1,46 @@
-import sys
+# -*- coding: utf-8 -*-
+import logging
+from logging import handlers
+from config import Config
 
 
 class Logger(object):
     """
-    日志打印
+    自定义封装logging模块
     """
-    def __init__(self, filename="default.log"):
-        self.terminal = sys.stdout
-        self.log = open(filename, "a")
-    
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-    
-    def flush(self):
-        pass
+
+    # 日志级别关系映射
+    level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+    }
+
+    def __init__(self, filename=Config().get('logger', 'file_name'), level=Config().get('logger', 'level')):
+        # 默认设置
+        fmt = '%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+        backCount = int(Config().get('logger', 'backCount'))
+        when = Config().get('logger', 'when')
+        self.logger = logging.getLogger(filename)
+        # 设置日志级别
+        self.logger.setLevel(self.level_relations.get(level))
+        # 设置日志格式
+        format_str = logging.Formatter(fmt)
+        # 往屏幕上输出
+        sh = logging.StreamHandler()
+        # 设置屏幕上显示的格式
+        sh.setFormatter(format_str)
+        # 往文件里写入，指定间隔时间自动生成文件的处理器
+        th = handlers.TimedRotatingFileHandler(filename=filename, when=when, backupCount=backCount, encoding='utf-8')
+        # 设置文件里写入的格式
+        th.setFormatter(format_str)
+        # 把对象加到logger里
+        self.logger.addHandler(sh)
+        self.logger.addHandler(th)
+
+
+if __name__ == '__main__':
+    log = Logger().logger
+    log.error('error')
