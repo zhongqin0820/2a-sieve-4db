71e89a197f3736baf16f06afe855e7503a956664
zhongqin0820 <zhongqin0820@163.com>
2019-08-31 21:34:30 +0800

    add random user-agent

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import configparser
+import json
+import random
 
 
 class Config(object):
@@ -10,6 +12,7 @@ class Config(object):
         """
         :param file_name: ini配置文件所在路径: /path/to/config.ini
         :type file_name: str
+        :raise: IndexError
         """
         super(Config, self).__init__()
         self.file_name = file_name
@@ -18,7 +21,7 @@ class Config(object):
             self.config.read(self.file_name)
             self.section = self.config.sections()[0]
         except Exception as e:
-            print(e)
+            raise e
 
     def use_section(self, section=''):
         """
@@ -40,13 +43,14 @@ class Config(object):
         :type option: str
         :return: 对应option的值
         :rtype: str
+        :raise: configparser.NoSectionError, AttributeError
         """
         try:
             if section is '':
                 section =  self.section
             return self.config.get(section=section, option=option)
         except Exception as e:
-            return ''
+            raise e
 
     def sections(self):
         """
@@ -61,13 +65,14 @@ class Config(object):
 
         :param section: ini配置文件中用[]标识的内容
         :type section: str
+        :raise: configparser.NoSectionError, AttributeError
         """
         try:
             if section is '':
                 section = self.section
             return self.config.options(section)
         except Exception as e:
-            return None
+            raise e
 
     def items(self, section=''):
         """
@@ -77,13 +82,33 @@ class Config(object):
         :type section: str
         :return: 对应section的所有<option,value>对
         :rtype: List[(str,str)]
+        :raise: configparser.NoSectionError, AttributeError
         """
         try:
             if section is '':
                 section = self.section
             return self.config.items(section)
         except Exception as e:
-            return None
+            raise e
+
+    def get_ua(self):
+        """
+        返回一个随机的User-Agent
+
+        :return: 一个User-Agent
+        :rtype: str
+        :raise: AttributeError
+        """
+        file_name = self.config.get('headers', 'user_agent_file')
+        try:
+            with open(file_name, 'r') as f:
+                user_agent = json.load(f)
+        except Exception as e:
+            raise e
+        else:
+            key = random.choice(list(user_agent.keys()))
+            ua = random.choice(user_agent[key])
+            return ua
 
 
 if __name__ == '__main__':
@@ -96,16 +121,19 @@ if __name__ == '__main__':
     password = root
     database = test
     """
+    # 测试随机读取ua
     c = Config()
-    s = c.get(section="mysql", option="host")
-    print(s)
-    s = c.get(section="mysql", option="port")
-    print(s)
-    # 设置section
-    c.use_section("mysql")
-    s = c.get(option="user")
-    print(s)
-    s = c.get(option="password")
-    print(s)
-    s = c.get(option="database")
-    print(s)
+    print(c.get_ua())
+    # # 测试其它操作
+    # s = c.get(section="mysql", option="host")
+    # print(s)
+    # s = c.get(section="mysql", option="port")
+    # print(s)
+    # # 设置section
+    # c.use_section("mysql")
+    # s = c.get(option="user")
+    # print(s)
+    # s = c.get(option="password")
+    # print(s)
+    # s = c.get(option="database")
+    # print(s)
